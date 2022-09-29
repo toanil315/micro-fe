788c9600e0fcff48de35f6d024f4b7cae4c26ebb
toanil315 <congtoan.dn@gmail.com>
2022-09-30 00:48:41 +0700

    create container deploy script

--- packages/container/configs/webpack.dev.js
+++ packages/container/configs/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfigs = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
@@ -20,9 +19,6 @@ const devConfigs = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
